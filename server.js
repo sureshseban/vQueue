5bdc2c5943710e92e3d9a47774ed2875679317d3
sureshseban <suresh.vgknair@gmail.com>
2020-07-29 15:46:50 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+
+let mysql = require('mysql');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
+})
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'mysql@123',
+    database: 'virtualqueue'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+});
+
+app.get('/process_get', function (req, res) {
+    response = {
+        first_name: 'John',
+        last_name: 'Mathews'
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+    console.log(req);
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+    console.log(__dirname);
+    console.log('http://127.0.0.1:8081/');
+})
