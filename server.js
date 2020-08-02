66940a17187dc18b2ecdef225f569f4401c55bc2
sureshseban <suresh.vgknair@gmail.com>
2020-08-02 21:48:31 +0530

    switched to post 80

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post('/process_post', urlencodedParser, function (req, res) {
     res.end(JSON.stringify(response));
 })
 
-var server = app.listen(5000, function () {
+var server = app.listen(80, function () {
     console.log(__dirname);
     console.log('http://127.0.0.1:5000/');
 })
