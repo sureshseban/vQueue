9479a09a39e94e532926fa58beaebc8a2e4f6049
sureshseban <suresh.vgknair@gmail.com>
2020-08-12 21:34:24 +0530

    code cleanup in server file

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var app = express();
-
-let mysql = require('mysql');
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
-
+const app = express();
 const swaggerOptions = {
     swaggerDefinition: {
         info: {
@@ -20,16 +17,10 @@ const swaggerOptions = {
     },
     apis: ['server.js']
 }
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/" + "index.html");
-})
 
 let connection = mysql.createConnection({
     host: 'weq.csbuoireovxd.ap-south-1.rds.amazonaws.com',
@@ -45,36 +36,8 @@ connection.connect(function (err) {
     console.log('Connected to the MySQL server.');
 });
 
-app.get('/process_getdata', function (req, res) {
-    connection.query('CALL new_procedure', function (error, results, fields) {
-        if (error) {
-            throw error
-        } else {
-            res.json(results)
-        }
-    })
-})
-
-app.get('/process_postdata', function (req, res) {
-    let obj = { fname: '', lname: '' }
-    connection.query('CALL new_procedureFORINSERT', [obj], function (error, results, fields) {
-        if (error) {
-            throw error
-        } else {
-            res.json(results)
-        }
-    })
-})
-
-app.get('/process_updatedata', function (req, res) {
-    let id = 1;
-    connection.query('CALL new_procedureFORUPDATE', [id], function (error, results, fields) {
-        if (error) {
-            throw error
-        } else {
-            res.json(results)
-        }
-    })
+app.listen(80, function () {
+    console.log('server started - http://127.0.0.1:80/');
 })
 
 /**
@@ -92,65 +55,9 @@ app.get('/process_updatedata', function (req, res) {
  *         type: number
  */
 
-// /**
-//  * @swagger
-//  * /process_get:
-//  *   get:
-//  *      tags:
-// *           - api
-//  *      description: use to get process information
-//  *      produces:
-//  *          - application/json
-//  *      consumes:
-//  *          - application/json
-//  *      responses:
-//  *          '200':
-//  *              schema:
-//  *                  $ref: '#/definitions/Name'
-//  *              description: A succesful response
-//  */
-app.get('/process_get', function (req, res) {
-    response = {
-        first_name: 'John',
-        last_name: 'Mathews'
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
-})
-
-
-// /**
-//  * @swagger
-//  * /process_post:
-//  *   post:
-//  *     tags:
-//  *       - api
-//  *     description: returns full name
-//  *     produces:
-//  *       - application/json
-//  *     parameters:
-//  *       - name: puppy
-//  *         description: Name object
-//  *         in: body
-//  *         required: true
-//  *         schema:
-//  *           $ref: '#/definitions/Name'
-//  *     responses:
-//  *       200:
-//  *         description: Successfully created
-//  */
-app.post('/process_post', function (req, res) {
-    response = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name
-    };
-    console.log(response.first_name + ' ' + response.last_name);
-    res.end(JSON.stringify(response.first_name + ' ' + response.last_name));
-})
-
 /**
  * @swagger
- * /branchdetails:
+ * /getbranchdetail:
  *   post:
  *     tags:
  *       - Branch Details
@@ -167,7 +74,7 @@ app.post('/process_post', function (req, res) {
  *       200:
  *         description: Success
  */
-app.post('/branchdetails', function (req, res) {
+app.post('/getbranchdetail', function (req, res) {
     const R = 6371e3;
     const cos = Math.cos;
     const pie = Math.PI;
@@ -176,10 +83,10 @@ app.post('/branchdetails', function (req, res) {
     const lat = Number(req.body.lat)
     const long = Number(req.body.long)
 
-    const MinLatitude = lat - radius / R * 180 / pie; // 0.00000000
-    const MaxLatitude = lat + radius / R * 180 / pie; // 0.00000000
-    const MinLongitude = (long - radius / R * 180 / pie) / (cos(lat * pie / 180)) // 0.00000000
-    const MaxLongitude = (long + radius / R * 180 / pie) / (cos(lat * pie / 180)) // 0.00000000
+    const MinLatitude = lat - radius / R * 180 / pie;
+    const MaxLatitude = lat + radius / R * 180 / pie;
+    const MinLongitude = (long - radius / R * 180 / pie) / (cos(lat * pie / 180));
+    const MaxLongitude = (long + radius / R * 180 / pie) / (cos(lat * pie / 180));
     connection.query("CALL GetBranchDetail('" + UserID + "', '" + MinLatitude + "', '" + MaxLatitude + "', '" + MinLongitude + "', '" + MaxLongitude + "')", function (error, results, fields) {
         if (error) {
             throw error
@@ -187,8 +94,4 @@ app.post('/branchdetails', function (req, res) {
             res.json(results.length ? results[0] : []);
         }
     })
-})
-
-app.listen(80, function () {
-    console.log('server started - http://127.0.0.1:80/');
 })
