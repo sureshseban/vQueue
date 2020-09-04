fba46c147365c96724ea5fd0cc574088ae67f5f2
sureshseban <suresh.vgknair@gmail.com>
2020-09-04 11:24:42 +0530

    admin api integration for slot details

--- Routes/admin/slot.route.js
+++ Routes/admin/slot.route.js
@@ -8,16 +8,12 @@ router.post('/details', async (req, res, next) => {
         const { userID, branchID, startDate, endDate } = req.body
         connection.query("CALL GetAdminSlotDetail('" + userID + "', '" + branchID + "', '" + startDate + "', '" + endDate + "')", (error, results) => {
             if (error) { next(createError.InternalServerError()) } else {
-                res.json({ data: results.length ? results[0] : [], config: results.length > 1 ? results[1] : {} })
+                res.json({ booked: results.length ? results[0] : [], freeSlots: results.length > 1 ? results[1] : [], config: results.length > 2 ? results[2] : {} })
             }
         })
     } catch (error) {
         next(error)
     }
-})
-
-router.post('/test', async (req, res, next) => {
-    res.json("hello world")
 })
 
 module.exports = router
