8b44760be502af177961e6c8a47938e4774a1fee
sureshseban <suresh.vgknair@gmail.com>
2020-10-27 22:00:54 +0530

    scripts version updated in to 3.4.3

---
+++ build/precache-manifest.4b983ce799c4e4f3ee3b7a952dc2f244.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08bb8a4e3474d3ab58fd02981457046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec1bc37c9ceea65d9e27",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "fdc394129b78fc94aaa0",
+    "url": "/static/css/main.7511e98e.chunk.css"
+  },
+  {
+    "revision": "ec1bc37c9ceea65d9e27",
+    "url": "/static/js/2.2672dd5f.chunk.js"
+  },
+  {
+    "revision": "be3ae91867bd4c806afeb90c76d5e54f",
+    "url": "/static/js/2.2672dd5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc394129b78fc94aaa0",
+    "url": "/static/js/main.dafa4cae.chunk.js"
+  },
+  {
+    "revision": "b356f625be4accf4ab4f",
+    "url": "/static/js/runtime-main.1ba291e1.js"
+  },
+  {
+    "revision": "bb4ed5c4834ee7b97e0d287ed8950b9d",
+    "url": "/static/media/Pointer.bb4ed5c4.svg"
+  },
+  {
+    "revision": "0f4db4a505b31e78440ddaaa00acfe5d",
+    "url": "/static/media/Shopping Cart-ico.0f4db4a5.svg"
+  },
+  {
+    "revision": "081c1cf85df84355d815911a060fed75",
+    "url": "/static/media/WEQ-logo.081c1cf8.svg"
+  },
+  {
+    "revision": "1b220f77158f48f5e9258355e08c7306",
+    "url": "/static/media/bgImage.1b220f77.svg"
+  },
+  {
+    "revision": "8239e13472b399111ac74d67b57d6024",
+    "url": "/static/media/user.8239e134.svg"
+  }
+]);
